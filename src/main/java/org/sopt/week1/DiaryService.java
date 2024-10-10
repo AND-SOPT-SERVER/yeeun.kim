459021761bb5d342a0ab64b0aba36667535e9910
yeeun0702 <kimyeeun0702@naver.com>
2024-10-10 17:34:55 +0900

    feat : String 타입의 id를 Long으로 변환과정을 parseId 메서드로 분리

--- src/main/java/org/sopt/week1/DiaryService.java
+++ src/main/java/org/sopt/week1/DiaryService.java
@@ -5,7 +5,6 @@ import java.util.List;
 public class DiaryService {
     private final DiaryRepository diaryRepository = new DiaryRepository();
 
-
     void writeDiary(final String body) {
         diaryRepository.save(body);
     }
@@ -15,35 +14,27 @@ public class DiaryService {
     }
 
     void patchDiary(String id, String newBody) {
-        Long longId; // 매개변수 String으로 받은 id 값을 Long 타입으로 바꾼 후 저장할 변수 선언
-        try {
-            longId = Long.parseLong(id); // String을 Long으로 변환
-        } catch (NumberFormatException e) { // 예외 발생 시 예외처리
-            throw new IllegalArgumentException();
-        }
-        diaryRepository.patch(longId, newBody); // 변환된 Long ID를 Repository에 전달
+        Long longId = parseId(id);
+        diaryRepository.patch(longId, newBody);
     }
 
     void deleteDiary(final String id) {
-        Long longId;
-        try {
-            longId = Long.parseLong(id); // String을 Long으로 변환
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException();
-        }
-        diaryRepository.delete(longId); // 변환된 Long ID를 리포지토리에 전달
-
+        Long longId = parseId(id);
+        diaryRepository.delete(longId);
     }
 
     // todo : 삭제된 내용 복구하는 기능, 기존 id와 함께 복구되도록 설정할 것 .
     // todo : RESTORE 명령어를 통해서 복구
     void restoreDiary(String id) {
-        Long longId;
+        Long longId = parseId(id);
+        diaryRepository.restore(longId);
+    }
+
+    private Long parseId(String id) {
         try {
-            longId = Long.parseLong(id);
+            return Long.parseLong(id); // String을 Long으로 변환
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("Invalid ID format");
+            throw new IllegalArgumentException();
         }
-        diaryRepository.restore(longId); // 변환된 Long ID를 Repository에 전달
     }
 }
